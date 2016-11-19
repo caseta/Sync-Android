db6c4936544712a32268e0b236acab4c6f7525c6
Taylor Case <case.161@osu.edu>
2016-11-19 12:38:20 -0500

    Formatting

--- app/src/main/java/com/sync/taylorcase/sync/Home/HomeActivity.java
+++ app/src/main/java/com/sync/taylorcase/sync/Home/HomeActivity.java
@@ -1,13 +1,15 @@
-package com.sync.taylorcase.sync;
+package com.sync.taylorcase.sync.Home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.sync.taylorcase.sync.R;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.view_home);
     }
 }
