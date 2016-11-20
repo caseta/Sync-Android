49c8815d6324cb6ced6c7dad90e2503bb71a7a20
Taylor Case <case.161@osu.edu>
2016-11-19 21:19:13 -0500

    Added MySyncsActivity

--- app/src/main/java/com/sync/taylorcase/sync/JoinGroup/JoinGroupActivity.java
+++ app/src/main/java/com/sync/taylorcase/sync/JoinGroup/JoinGroupActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -15,6 +16,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.sync.taylorcase.sync.GroupUser;
 import com.sync.taylorcase.sync.MySyncs.MySyncsActivity;
 import com.sync.taylorcase.sync.R;
 
@@ -25,12 +27,14 @@ import butterknife.ButterKnife;
 
 public class JoinGroupActivity extends AppCompatActivity {
 
-    @Bind(R.id.choose_items_done_button) Button doneButton;
+    @Bind(R.id.choose_items_done_button)
+    Button doneButton;
 
     ListView listView;
     DatabaseReference database;
     FirebaseAuth auth;
     ArrayList<String> groupNames;
+    ArrayList<String> userIdsInGroup;
     Context context;
 
     @Override
@@ -52,15 +56,16 @@ public class JoinGroupActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                findViewById(R.id.choose_loading).setVisibility(View.VISIBLE);
+//                findViewById(R.id.choose_loading).setVisibility(View.VISIBLE);
+
+                joinTheGroup(groupNames.get(position));
 
                 calculateSyncs(groupNames.get(position));
 
-                findViewById(R.id.create_loading).setVisibility(View.GONE);
+//                findViewById(R.id.create_loading).setVisibility(View.GONE);
                 navigateToSyncs();
             }
         });
-
     }
 
     public void populateDataArray() {
@@ -70,7 +75,7 @@ public class JoinGroupActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 groupNames = new ArrayList<String>();
 
-                for (DataSnapshot group: dataSnapshot.getChildren()) {
+                for (DataSnapshot group : dataSnapshot.getChildren()) {
                     String groupName = group.getKey();
                     groupNames.add(groupName);
                 }
@@ -97,11 +102,55 @@ public class JoinGroupActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void calculateSyncs(String groupName) {
+    public void joinTheGroup(final String groupName) {
+        final String userId = auth.getCurrentUser().getUid();
 
+        database.child("users").child(userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String firstName = dataSnapshot.child("firstName").getValue().toString();
+
+                GroupUser groupUser = new GroupUser(firstName);
+
+                database.child("groups").child(groupName).child(userId).setValue(groupUser);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
+    public void calculateSyncs(String groupName) {
 
+        database.child("groups").child(groupName).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String currentUserId = auth.getCurrentUser().getUid();
+
+                userIdsInGroup = new ArrayList<String>();
+
+                for (DataSnapshot user : dataSnapshot.getChildren()) {
+                    String userId = user.getKey();
+                    if (userId != currentUserId && currentUserId != "show") {
+                        userIdsInGroup.add(userId);
+                    }
+                }
+
+                Log.e("YEET_TAG", "userIds: in grou: " + userIdsInGroup);
+                // We have all user IDs, time to match
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // TODO: error
+            }
+        });
+
+    }
 
 
 }
